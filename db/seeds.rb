0a282091c9aad202bd78fee3a4e9bb424e5ce9be
jrehakTow <jrehak1@students.towson.edu>
2016-02-29 17:18:52 -0500

    Change database to postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.delete_all
+Item.create(:name=>"Lab", :description=>"Seed the database and finish the question.", :due_date=>"2016-02-29 23:59:00")
+Item.create(:name=>"wash car", :description=>"Use the special soap", :due_date=>"2016-03-01 22:45:00")
